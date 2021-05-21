3c97247f65ef2dff0e18fdd7d27c9f76f32a2722
KornelWitkowski <mrkhornel@gmail.com>
2021-05-21 20:55:35 +0200

    Ready app

--- cashmachine/main.py
+++ cashmachine/main.py
@@ -22,7 +22,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.setGeometry(300, 300, 800, 600)
+        self.setGeometry(300, 300, 500, 450)
         self.setWindowTitle("Cash machine")
         self.setWindowIcon(QIcon("iconMain.jpg"))
 
@@ -83,9 +83,11 @@ class LoginWidget(QWidget):
 
         if answer.decode() == "Ok":
             self.setParent(None)
-            Parent.LoggedinWin = LoggedinWindow(self)
-            Parent.LoggedinWin.setFixedSize(500, 250)
-            Parent.layout.addWidget(Parent.LoggedinWin, *(1, 1, 1, 1))
+            Parent.LoggedinWin = LoggedinWindow(Parent)
+            Parent.LoggedinWin.setFixedSize(320, 400)
+            Parent.layout.addWidget(Parent.LoggedinWin)
+        else:
+            QMessageBox.about(self, "Information", answer.decode())
 
 
 class LoggedinWindow(QWidget):
@@ -96,26 +98,29 @@ class LoggedinWindow(QWidget):
         s.send(str.encode("Give me all that you got now!"))
         answer = s.recv(BUFFER_SIZE)
 
-        #DecodedData = ast.literal_eval(answer.decode()) Czemu to nie działa?
         json_acceptable_string = answer.decode().replace("'", "\"")
         Data = json.loads(json_acceptable_string)
 
         self.layout = QGridLayout(self)
         self.labelTime = QLabel(QDateTime.currentDateTime().toString('yyyy-MM-dd hh:mm:ss'))
+        self.labelTime.setFont(QFont('Arial', 12))
         self.timer = QTimer()
         self.timer.timeout.connect(self.show_time)
-        self.layout.addWidget(self.labelTime, 0, 1, 1, 2)
+        self.layout.addWidget(self.labelTime, 0, 0, 1, 2)
         self.timer.start(1000)
 
         self.labelName = QLabel(Data["name"] + " " + Data["surname"])
-        self.layout.addWidget(self.labelName, 1, 1, 1, 2)
+        self.labelName.setFont(QFont('Arial', 12))
+        self.layout.addWidget(self.labelName, 1, 0, 1, 2)
 
         self.labelBalance = QLabel("Balance:  " + str(Data["balance"]))
-        self.layout.addWidget(self.labelBalance, 2, 1, 1, 2)
+        self.labelBalance.setFont(QFont('Arial', 12))
+        self.layout.addWidget(self.labelBalance, 2, 0, 1, 2)
 
         self.textboxAmount = QLineEdit(self)
-        self.textboxAmount.setFixedSize(100, 25)
-        self.layout.addWidget(self.textboxAmount, *(3, 1, 1, 1))
+        self.textboxAmount.setFixedSize(303, 40)
+        self.textboxAmount.setFont(QFont('Arial', 12))
+        self.layout.addWidget(self.textboxAmount, *(3, 0, 1, 1))
 
         btns = {(4, 0): "Deposit", (4, 1): "Payout",
                 (5, 0): "History", (5, 1): "Data",
@@ -126,14 +131,14 @@ class LoggedinWindow(QWidget):
             btn = QPushButton()
             btn.setText(name)
             btn.clicked.connect(self.push_button)
+            btn.setFixedSize(150, 45)
             btn.setObjectName(name)
             self.layout.addWidget(btn, x, y)
 
         self.setBtn = QPushButton(text='Log out')
-        self.setBtn.setFont(QFont('Arial', 14))
-        self.setBtn.setFixedSize(200, 45)
+        self.setBtn.setFixedSize(303, 45)
         self.setBtn.clicked.connect(self.log_out)
-        self.layout.addWidget(self.setBtn, *(8, 0))
+        self.layout.addWidget(self.setBtn, *(8, 0, 1, 1))
 
     def push_button(self):
         sendingButtonName = self.sender().objectName()
@@ -142,9 +147,9 @@ class LoggedinWindow(QWidget):
         if sendingButtonName == "Deposit":
 
             time = QDateTime.currentDateTime()
-            timeDisplay = time.toString('yyyy-MM-dd hh:mm:ss dddd')
+            timeDisplay = time.toString('yyyy-MM-dd hh:mm:ss')
             try:
-                if int(amount) >= 0:
+                if int(amount) > 0:
                     data = {"transaction type": "Deposit", "amount": amount, "transaction time": timeDisplay}
                     s.send(str.encode(str(data)))
                     answer = s.recv(BUFFER_SIZE)
@@ -157,9 +162,9 @@ class LoggedinWindow(QWidget):
         elif sendingButtonName == "Payout":
 
             time = QDateTime.currentDateTime()
-            timeDisplay = time.toString('yyyy-MM-dd hh:mm:ss dddd')
+            timeDisplay = time.toString('yyyy-MM-dd hh:mm:ss')
             try:
-                if int(amount) >= 0:
+                if int(amount) > 0:
                     data = {"transaction type": "Payout", "amount": amount, "transaction time": timeDisplay}
                     s.send(str.encode(str(data)))
                     answer = s.recv(BUFFER_SIZE)
@@ -188,8 +193,14 @@ class LoggedinWindow(QWidget):
             s.send(str.encode(str("Show data")))
             answer = s.recv(BUFFER_SIZE)
             DecodedData = ast.literal_eval(answer.decode())
-            self.DataWin = DataWindow(DecodedData)
+            self.DataWin = DataWindow(DecodedData, parent=self)
             self.DataWin.show()
+
+        elif sendingButtonName == "History":
+            s.send(str.encode(str("History")))
+            answer = s.recv(BUFFER_SIZE)
+            self.HistoryWin = HistoryWindow(answer.decode(), parent=self)
+            self.HistoryWin.show()
 
     def show_time(self):
         time = QDateTime.currentDateTime()
@@ -216,11 +227,11 @@ class LogoutWidget(QWidget):
         self.labelName.setAlignment(Qt.AlignLeft)
         self.layout.addWidget(self.labelName, *(1, 0))
 
-        self.bt1 = QPushButton(text='Zamknij')
-        self.bt1.setFont(QFont('Arial', 14))
-        self.bt1.setFixedSize(200, 45)
-        self.bt1.clicked.connect(self.close)
-        self.layout.addWidget(self.bt1, *(8, 0))
+        self.closeBtn = QPushButton(text='Close')
+        self.closeBtn.setFont(QFont('Arial', 14))
+        self.closeBtn.setFixedSize(200, 45)
+        self.closeBtn.clicked.connect(self.close)
+        self.layout.addWidget(self.closeBtn, *(8, 0))
 
         return
 
@@ -228,17 +239,17 @@ class LogoutWidget(QWidget):
         Parent = self.parent().parent()
         self.setParent(None)
 
-        Parent.LoginWin.textboxLogin.clear()
-        Parent.LoginWin.textboxPassword.clear()
+        Parent.LoginWid.textboxLogin.clear()
+        Parent.LoginWid .textboxPassword.clear()
 
-        Parent.layout.addWidget(Parent.LoginWin, *(1, 1, 1, 1))
+        Parent.layout.addWidget(Parent.LoginWid, *(1, 1, 1, 1))
 
 
 class ChangePasswordWindow(QMainWindow):
     def __init__(self):
         super(ChangePasswordWindow, self).__init__()
 
-        self.setGeometry(300, 300, 600, 600)
+        self.setGeometry(300, 300, 350, 300)
         self.setWindowTitle("Change password")
         self.setWindowIcon(QIcon("iconChangePassword.jpg"))
 
@@ -248,7 +259,7 @@ class ChangePasswordWindow(QMainWindow):
         self.setCentralWidget(self.frame)
 
         self.loginWin = ChangePasswordWidget(self)
-        self.loginWin.setFixedSize(500, 250)
+        self.loginWin.setFixedSize(250, 250)
         self.layout.addWidget(self.loginWin, *(1, 1, 1, 1))
 
         self.show()
@@ -322,8 +333,8 @@ class ChangePasswordWidget(QWidget):
 
 
 class DataWindow(QMainWindow):
-    def __init__(self, data):
-        super(DataWindow, self).__init__()
+    def __init__(self, data, parent=None):
+        super(DataWindow, self).__init__(parent)
 
         self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle("Account data")
@@ -339,9 +350,56 @@ class DataWindow(QMainWindow):
             self.label.setAlignment(Qt.AlignLeft)
             self.layout.addWidget(self.label, *(count, 0, 1, 1))
 
-
         self.show
 
+
+class HistoryWindow(QMainWindow):
+    def __init__(self, data, parent=None):
+        super(HistoryWindow, self).__init__(parent)
+
+        self.frame = QFrame(self)
+        self.layout = QGridLayout()
+        self.frame.setLayout(self.layout)
+        self.setCentralWidget(self.frame)
+
+        list1 = []
+        list2 = data.split('\n')
+
+        for row in list2:
+            list1.append(row.split(' '))
+
+        data = list1
+
+        self.setGeometry(400, 300, 500, 400)
+        self.setWindowTitle("History")
+
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(3)
+
+        self.tableWidget.setHorizontalHeaderLabels(["Type", "Amount", "Time"])
+
+        header = self.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(2, QHeaderView.Stretch)
+
+        if data == [['', '']]:
+            self.tableWidget.setRowCount(0)
+        else:
+            self.tableWidget.setRowCount(len(data))
+
+        for count1, row in enumerate(data):
+            for count2, cell in enumerate(row):
+                if count2 < 2:
+                    self.tableWidget.setItem(count1, count2, QTableWidgetItem(str(cell)))
+                elif count2 == 2:
+                    self.tableWidget.setItem(count1, count2, QTableWidgetItem(str(cell) + " "+ str(row[3])))
+                else:
+                    continue
+
+        self.tableWidget.move(0, 0)
+        self.layout.addWidget(self.tableWidget, *(0, 0, 1, 1))
+
+        self.show
 
 
 if __name__ == '__main__':
